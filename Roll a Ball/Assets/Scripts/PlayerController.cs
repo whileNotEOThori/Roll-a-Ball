6aa05c7f894152f6a7618b48530374f3d5cce4bb
whileNotEOThori <140858944+whileNotEOThori@users.noreply.github.com>
2024-10-20 13:34:18 +0200

    Set the movement vector, assign a new Rigidbody variable, and add a FixedUpdate function

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
     {
 
     }
 
     void onMove(InputValue movementValue)
     {
-
+        Vector2 movementVector = movementValue.Get<Vector2>();
     }
+
 
 }
